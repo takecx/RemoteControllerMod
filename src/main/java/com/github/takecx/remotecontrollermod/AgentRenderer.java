9db7914b581ce8a0b8c4fa83a78db1fe56d71e38
takeshi <panda531531@gmail.com>
2021-04-04 08:10:51 +0900

    remove unused code

--- src/main/java/com/github/takecx/remotecontrollermod/AgentRenderer.java
+++ src/main/java/com/github/takecx/remotecontrollermod/AgentRenderer.java
@@ -1,23 +1,5 @@
 package com.github.takecx.remotecontrollermod;
 
-//import net.minecraft.client.render.entity.EntityRenderDispatcher;
-//import net.minecraft.client.render.entity.MobEntityRenderer;
-//import net.minecraft.client.render.entity.model.CreeperEntityModel;
-//import net.minecraft.util.Identifier;
-//
-//public class AgentRenderer extends MobEntityRenderer<AgentEntity, CreeperEntityModel<AgentEntity>> {
-//    public AgentRenderer(EntityRenderDispatcher entityRenderDispatcher_1)
-//    {
-//        //モデルを指定
-//        super(entityRenderDispatcher_1, new CreeperEntityModel<>(), 1);
-//    }
-//    @Override
-//    public Identifier getTexture(AgentEntity entity) {
-//        //テクスチャの場所を指定
-//        return new Identifier("example", "textures/entity/example_entity.png"); //ここも以前のバージョン(1.15.2まで)とは違います。
-//    }
-//}
-
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.entity.Entity;
