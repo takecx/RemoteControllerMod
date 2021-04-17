f17012adb4ff4dcf6f5346b5cd879ee1daaf23f6
takeshi <panda531531@gmail.com>
2021-04-17 11:41:11 +0900

    add ItemList class

---
+++ src/main/java/com/github/takecx/remotecontrollermod/lists/ItemList.java
@@ -0,0 +1,8 @@
+package com.github.takecx.remotecontrollermod.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+    public static Item StarIngot;
+    public static Item StarBlock;
+}
