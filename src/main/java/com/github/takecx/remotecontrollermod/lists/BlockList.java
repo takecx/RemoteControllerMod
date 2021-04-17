282440626a9d763d8b0cc65532544218749dd9c7
takeshi <panda531531@gmail.com>
2021-04-17 11:40:59 +0900

    add BlockList class

---
+++ src/main/java/com/github/takecx/remotecontrollermod/lists/BlockList.java
@@ -0,0 +1,8 @@
+package com.github.takecx.remotecontrollermod.lists;
+
+import com.github.takecx.remotecontrollermod.blocks.StarBlock;
+import net.minecraft.block.Block;
+
+public class BlockList {
+    public static StarBlock StarBlock = new StarBlock();
+}
