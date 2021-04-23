c1d9754308c1008ca3e1d7418bfb474b57ab61f7
takeshi <panda531531@gmail.com>
2021-04-23 18:57:55 +0900

    update construct stage logic

--- src/main/java/com/github/takecx/remotecontrollermod/stages/Stage1_1.java
+++ src/main/java/com/github/takecx/remotecontrollermod/stages/Stage1_1.java
@@ -1,6 +1,8 @@
 package com.github.takecx.remotecontrollermod.stages;
 
 import com.github.takecx.remotecontrollermod.AgentEntity;
+import com.github.takecx.remotecontrollermod.blocks.StarBlock;
+import com.github.takecx.remotecontrollermod.lists.BlockList;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
@@ -8,14 +10,20 @@ import net.minecraft.world.server.ServerWorld;
 
 public class Stage1_1 extends BaseStage{
 
-    public Stage1_1(String stageIDIn){
-        super(stageIDIn);
+    public Stage1_1(String stageIDIn,Integer stageXIn,Integer stageYIn,Integer stageZIn){
+        super(stageIDIn,stageXIn,stageYIn,stageZIn);
     }
 
     @Override
-    void ConstructStageContents(Vector3d areaVec, Vector3d referencePos, ServerWorld worldIn, AgentEntity myAgent) {
-        ConstructGround(areaVec,referencePos,worldIn);
-        Vector3d agentStartPos = new Vector3d(referencePos.x + areaVec.x / 2 - 1,referencePos.y + 2,referencePos.z + 2);
+    void ConstructStageContents(Vector3d referencePos, ServerWorld worldIn, AgentEntity myAgent) {
+        ConstructGround(this.stageAreaVec,referencePos,worldIn);
+        Vector3d agentStartPos = new Vector3d(referencePos.x + this.stageAreaVec.x / 2,referencePos.y + 2,referencePos.z + 2);
         AgentEntity.SummonAgent(agentStartPos,worldIn,myAgent);
+        worldIn.setBlockState(new BlockPos(agentStartPos.x,agentStartPos.y,agentStartPos.z + 6), BlockList.StarBlock.getDefaultState());
+
+        //
+        for (int i = 0; i < 7; i++) {
+            worldIn.setBlockState(new BlockPos(agentStartPos.x,agentStartPos.y - 1,agentStartPos.z + i), Blocks.COARSE_DIRT.getDefaultState());
+        }
     }
 }
