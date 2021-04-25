e21fdbd5bec13b95d893ca6a758af8dfe34fcca8
takeshi <panda531531@gmail.com>
2021-04-26 00:17:40 +0900

    add Stage1_2(turn right), 1_3(turn left), 1_4(turn back)

--- src/main/java/com/github/takecx/remotecontrollermod/lists/StageList.java
+++ src/main/java/com/github/takecx/remotecontrollermod/lists/StageList.java
@@ -1,7 +1,6 @@
 package com.github.takecx.remotecontrollermod.lists;
 
-import com.github.takecx.remotecontrollermod.stages.BaseStage;
-import com.github.takecx.remotecontrollermod.stages.Stage1_1;
+import com.github.takecx.remotecontrollermod.stages.*;
 
 import java.util.HashMap;
 
@@ -10,6 +9,9 @@ public class StageList {
     static public HashMap<String, BaseStage> Stages = new HashMap<String, BaseStage>(){
         {
             put("1_1",new Stage1_1("1_1",11,11,11));
+            put("1_2",new Stage1_2("1_2",11,11,11));
+            put("1_3",new Stage1_3("1_3",11,11,11));
+            put("1_4",new Stage1_4("1_4",11,11,11));
         }
     };
 }
